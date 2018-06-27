bd35aa88ad7d5cca8a64f7f5d397221e3497f7ef
gatech0723 <shawngit123@gmail.com>
2018-06-26 21:57:26 -0700

    Initial Commit

---
+++ routes/api/users.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+//  Load User model
+const User = require("../../models/User");
+
+// @route  GET api/users/test
+// @desc   Tests users route
+// @access public
+router.get("/test", (req, res) =>
+  res.json({
+    msg: "Users Works"
+  })
+);
+
+//  @route GET api/users/register
+//  @desc  Register user
+//  @access Public
+router.post("/register", (req, res) => {
+  User.findOne({
+    email: req.body.email
+  });
+});
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
+
+const app = express();
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello Shawn"));
+
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
