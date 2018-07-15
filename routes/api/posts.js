8ea14c579b40dead03cacc7390cb3428eb8c4823
gatech0723 <shawngit123@gmail.com>
2018-07-14 22:17:12 -0700

    Add Post Create Route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+//Post model
+const Post = require("../../models/Post");
+
+//Validation
+const validatePostInput = require("../../validation/post");
+
 // @route  GET api/posts/test
 // @desc   Tests post route
 // @access public
@@ -12,4 +18,28 @@ router.get("/test", (req, res) =>
   })
 );
 
+// @route  POST api/posts/test
+// @desc   Create post
+// @access Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
 module.exports = router;
